e60062b490c351e90333cd9b2d3ce70717bdb3d5
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2024-12-05 08:41:43 +0100

    use pytest to write test cases

--- tests/signal_registry_test.py
+++ tests/signal_registry_test.py
@@ -9,50 +9,49 @@ try:
 except ImportError:
     from mock import MagicMock
 
-from pythonic_testcase import *
+import pytest
 
 from schwarz.puzzle_plugins.signal_registry import (connect_signals,
     SignalRegistry)
 
 
-class SignalRegistryTest(PythonicTestCase):
-    def test_call_plugin(self):
-        registry = SignalRegistry()
-        assert_none(registry.call_plugin('foo', signal_kwargs={'a': 137}),
-            message='return None if no plugin did subscribe for the signal')
+def test_call_plugin():
+    registry = SignalRegistry()
+    # return None if no plugin did subscribe for the signal
+    assert registry.call_plugin('foo', signal_kwargs={'a': 137}) is None
 
-        plugin = lambda sender, a: (a+1)
-        connect_signals({'foo': plugin}, registry.namespace)
-        assert_equals(138, registry.call_plugin('foo', signal_kwargs={'a': 137}))
+    plugin = lambda sender, a: (a+1)
+    connect_signals({'foo': plugin}, registry.namespace)
+    assert registry.call_plugin('foo', signal_kwargs={'a': 137}) == 138
 
-        plugin2 = lambda sender, a: (a+5)
-        connect_signals({'foo': plugin2}, registry.namespace)
-        assert_none(registry.call_plugin('foo', signal_kwargs={'a': 137}),
-            message='return None if multiple receivers are subscribed')
+    plugin2 = lambda sender, a: (a+5)
+    connect_signals({'foo': plugin2}, registry.namespace)
+    # return None if multiple receivers are subscribed
+    assert registry.call_plugin('foo', signal_kwargs={'a': 137}) is None
 
-    def test_call_plugins(self):
-        registry = SignalRegistry()
 
-        # only one plugin returns a value which is the final return value
-        plugin = MagicMock(return_value=None, spec={})
-        connect_signals({'foo': plugin}, registry.namespace)
-        plugin2 = lambda sender, a: (a+1)
-        connect_signals({'foo': plugin2}, registry.namespace)
-        result = registry.call_plugins('foo', signal_kwargs={'a': 137}, expect_single_result=True)
-        assert_equals(138, result)
-        plugin.assert_called_once_with(None, a=137)
+def test_call_plugins():
+    registry = SignalRegistry()
 
-        # ability to return values from all plugins
-        plugin3 = MagicMock(side_effect=lambda sender, a: (a-1), spec={})
-        connect_signals({'foo': plugin3}, registry.namespace)
-        with assert_raises(ValueError, message='multiple receivers which return values'):
-            registry.call_plugins('foo', signal_kwargs={'a': 127}, expect_single_result=True)
-        assert_equals(2, plugin.call_count)
-        plugin3.assert_called_once_with(None, a=127)
+    # only one plugin returns a value which is the final return value
+    plugin = MagicMock(return_value=None, spec={})
+    connect_signals({'foo': plugin}, registry.namespace)
+    plugin2 = lambda sender, a: (a+1)
+    connect_signals({'foo': plugin2}, registry.namespace)
+    result = registry.call_plugins('foo', signal_kwargs={'a': 137}, expect_single_result=True)
+    assert result == 138
+    plugin.assert_called_once_with(None, a=137)
 
-        results = registry.call_plugins('foo', signal_kwargs={'a': 125}, expect_single_result=False)
-        assert_length(3, results)
-        assert_equals({124, 126, None}, {i[1] for i in results})
-        assert_equals(3, plugin.call_count)
-        assert_equals(2, plugin3.call_count)
+    # ability to return values from all plugins
+    plugin3 = MagicMock(side_effect=lambda sender, a: (a-1), spec={})
+    connect_signals({'foo': plugin3}, registry.namespace)
+    with pytest.raises(ValueError):  # multiple receivers which return values
+        registry.call_plugins('foo', signal_kwargs={'a': 127}, expect_single_result=True)
+    assert plugin.call_count == 2
+    plugin3.assert_called_once_with(None, a=127)
 
+    results = registry.call_plugins('foo', signal_kwargs={'a': 125}, expect_single_result=False)
+    assert len(results) == 3
+    assert {i[1] for i in results} == {124, 126, None}
+    assert plugin.call_count == 3
+    assert plugin3.call_count == 2
